7e22d33a1f585611dcb425f18e3a9e4bfdfa81ab
Levi Engel <leviengelr@gmail.com>
2026-01-23 23:54:45 -0500

    Add functionality to Edfinancial parser (WIP)
    Created sample data to use for testing Edfinancial parser, added further
    functionality to parser, and updated code to use modern JavaScript
    syntax

--- import_transactions.js
+++ import_transactions.js
@@ -1,4 +1,5 @@
-import { Transaction } from './records/transaction';
+import fs from 'fs';
+import { Transaction } from './records/transaction.js';
 
 /**
  * Parse account data specific to Edfinancial Loan Servicer.
@@ -12,26 +13,54 @@ import { Transaction } from './records/transaction';
  * Then, download the information as a .csv file.
  */
 function parseEdfinancialCSV(csvFilePath) {
-    const fs = require('fs');
 
     // Read the file
     fs.readFile(csvFilePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error("Error reading file:", err);
-        return;
-    }
+        if (err) {
+            console.error("Error reading file:", err);
+            return;
+        }
 
-    const lines = data.trim().split("\n");
-    const headers = lines[0].split(",");
+        let lines = data.trim().split("\n");
+        let headers = lines[0].split(",");
 
-    // Remove unnecessary DOCTYPE declaration from Date column header
-    headers[0] = headers[0].replace(/^<!DOCTYPE HTML PUBLIC "-\/\/W3C\/\/DTD HTML 4\.0 Transitional\/\/EN">/, "");
-    
+        // Remove unnecessary DOCTYPE declaration from Date column header
+        headers[0] = headers[0].replace(/^<!DOCTYPE[^>]*>/i, "");
+        let transactions = [];
+
+        lines.forEach(line => {
+            let test = false;
+
+            let transaction = line.split(",")
+            let [month, day, year] = transaction[0].split("/").map(Number);
+
+            console.log(transaction[7].replace(/[$\r]/g, ""))
+
+            transactions.push(new Transaction(
+                new Date(year, month - 1, day),             // Date
+                transaction[1],                             // LoanName (Loan)
+                transaction[2],                             // Description
+                parseMoney(transaction[3]),                 // Principal
+                parseMoney(transaction[4]),                 // Interest
+                parseMoney(transaction[5]),                 // Fees
+                parseMoney(transaction[6]),                 // UnpaidPincipalBalanceValue (Total)
+                parseMoney(transaction[7])                
+            ));
+        });
+
+        transactions.forEach(transaction => {
+            console.log(transaction);
+        });
     });
+}
+
+function parseMoney(value) {
+    if (!value) return 0; // avoid undefined
+    return parseFloat(value.replace(/[$,"\r]/g, ""));
 }
 
 /**
  * Uncomment function call below and insert file name to parse
  * Edfinancial CSV file
- */ 
-// parseEdfinancialCSV();
+ */
+parseEdfinancialCSV("./sample_data.csv");
